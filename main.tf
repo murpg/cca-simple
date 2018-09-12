f9ab995ea7d3c32b56daa7227a66b76804a5387c
murpg <georgemurphy@websbygeorge.com>
2018-09-12 17:44:29 +0200

    Change droplet name to distinguish lucee and adobe2016

--- main.tf
+++ main.tf
@@ -11,7 +11,7 @@ data "template_file" "cca-docker-compose-file" {
 }
 
 resource "digitalocean_droplet" "application" {
-  name = "cca-application"
+  name = "cca-application_lucee"
 
   image              = "${var.droplet_image}"
   region             = "${var.droplet_region}"
